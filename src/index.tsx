6675a7e99811d94deafcb5d60995f94626499ac1
pmaciej <63106694+pmaciej@users.noreply.github.com>
2021-11-17 18:32:58 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import "typeface-nunito-sans";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 const theme = createTheme({
   typography: {
-    fontFamily: "'Nunito Sans !important'"
+    fontFamily: "Nunito Sans"
   }
 });
 
